4393c6bcacfd58e07c35fcd703d7a17d19c5bcb4
Son Hoang <wingadium1@gmail.com>
2015-11-04 01:00:41 +0700

    init commit

---
+++ calculate.py
@@ -0,0 +1,25 @@
+__author__ = 'mk'
+
+
+def calculate(x,y,operator):
+    """
+    simple calculate function with 4 main operator
+    in this function I ask you to use if else instead of switch case
+    :param x: the first number
+    :param y: the second number
+    :param operator: i define that it's a integer from 1 to 4: 1 is add, 2 is minus, 3 is multi and 4 is divide
+    :return: result when after calculating x y with sign
+    """
+    return 0
+
+
+def main_function():
+    """
+    free edit this function as main method, but you have to commit change of this function to github
+    :return:
+    """
+    print calculate(1,2,3)
+
+
+
+main_function()
