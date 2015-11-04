ce1ff7110b977fdd22269309032d61f791b0da35
cubo0001 <tranthanhtung0709@gmail.com>
2015-11-04 02:59:01 -0800

    add1

--- calculate.py
+++ calculate.py
@@ -10,6 +10,10 @@ def calculate(x,y,operator):
     :param operator: i define that it's a integer from 1 to 4: 1 is add, 2 is minus, 3 is multi and 4 is divide
     :return: result when after calculating x y with sign
     """
+
+    if operator == 1:
+        return x + y
+
     return 0
 
 
@@ -18,7 +22,7 @@ def main_function():
     free edit this function as main method, but you have to commit change of this function to github
     :return:
     """
-    print calculate(1,2,3)
+    print(calculate(5,2,1))
 
 
 
